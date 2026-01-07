ca8c33f7299ad82b92ed511db689c80c8554b47d
Arthur Svinarenko <arthur.svinarenko@ab-soft.net>
2026-01-07 15:20:25 +0200

    remove test fix

--- app/components/core/footer/Footer.tsx
+++ app/components/core/footer/Footer.tsx
@@ -16,7 +16,7 @@ export default function Footer() {
         <footer id="contact" className={styles.footer}>
             <div className={styles.topPart}>
                 <div className={clsx(styles.linkPart, styles.footerHoverPart)}>
-                    <p>Want tooo see<br/>your idea live?<br/> <a href="#" className={styles.footerHoverPartLink}><span className="">Let’s talk.</span></a></p>
+                    <p>Want to see<br/>your idea live?<br/> <a href="#" className={styles.footerHoverPartLink}><span className="">Let’s talk.</span></a></p>
                     <span className={clsx("svg", styles.rowIcon)} >
                         <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.66154 34L0 30.3385L25.1077 5.23077H2.61538V0H34V31.3846H28.7692V8.89231L3.66154 34Z" fill="white"/>
