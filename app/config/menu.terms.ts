e727cd670965827e0f99a2f5180f9f15efc88d63
Arthur Svinarenko <arthur.svinarenko@ab-soft.net>
2026-01-17 15:15:31 +0200

    remove file for build

--- app/config/menu.terms.ts
+++
@@ -1,7 +0,0 @@
-import { MenuItem } from "./menuTypes";
-
-export const menuTerms: MenuItem[] = [
-  { href: "/privacy-policy", labelKey: "Privacy Policy" },
-  { href: "/terms-of-use", labelKey: "Terms of Use" },
-  { href: "/cookie-policy", labelKey: "Cookie Policy" }
-];
